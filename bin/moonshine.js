30ab78a129a557161daeafbbfa2c566c22a57d23
John P Bloch <johnpbloch@gmail.com>
2015-01-13 17:10:29 -0600

    Add a port option to the command

--- bin/moonshine.js
+++ bin/moonshine.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
-var moonshine = require("../src");
+var moonshine = require("../src"),
+		args = require('minimist')(process.argv.slice(2), {
+			string: ['port'],
+			alias : {port: 'p'}
+		});
 
-var connection = moonshine.connect();
+var port = parseInt(args.port, 10);
+if (isNaN(port) || 'number' !== typeof port || port < 1) {
+	port = 239;
+}
 
-connection.on('connect',function(){
-	connection.close();
+var connection = moonshine.connect(port, function () {
+	console.log('Connected');
+	this.close();
 });
