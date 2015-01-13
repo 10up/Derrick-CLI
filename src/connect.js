1756ed65c4a762e0bd79f568968f3236ca4581a1
John P Bloch <johnpbloch@gmail.com>
2015-01-13 16:18:43 -0600

    Improve readability of connect

--- src/connect.js
+++ src/connect.js
@@ -12,34 +12,47 @@ module.exports = connectToServer;
  */
 function connectToServer(port, onConnect) {
 	"use strict";
+
+	// Set up default arguments and handle optional args being omitted
 	if ("function" === typeof port) {
 		onConnect = port;
 		port = 239;
 	}
+
 	port = port || 239;
+
 	onConnect = onConnect || function () {
 	};
+
 	var location = url.format({
-				protocol: 'http',
-				hostname: 'moonshine',
-				port    : port
-			}),
-			socket = sockets[location] ? sockets[location].sock : undefined,
-			connecting = sockets[location] ? sockets[location].connecting : undefined;
+		protocol: 'http',
+		hostname: 'moonshine',
+		port    : port
+	});
+
+	var socket = sockets[location] ? sockets[location].sock : undefined;
+
+	var connecting = sockets[location] ? sockets[location].connecting : undefined;
+
 	if (socket && (socket.connected || connecting)) {
 		return socket;
 	}
+
 	sockets[location] = {
 		sock      : io(location),
 		connecting: true
 	};
+
 	socket = sockets[location].sock;
+
 	socket.on('connect', function () {
 		sockets[location].connecting = false;
 		onConnect.apply(socket, arguments);
 	});
+
 	socket.on('error', function () {
 		sockets[location].connecting = false;
 	});
+
 	return socket;
 }
